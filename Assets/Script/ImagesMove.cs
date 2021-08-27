d104cf5707be9700a4edfd6e5758f9fa75c669e5
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2021-08-27 15:24:39 +0700

    Bukti Belajar Pert - 04

---
+++ Assets/Script/ImagesMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImagesMove : MonoBehaviour
+{
+    int[] posX = new int[] { 0, -18, -36, -54};
+    public int idx = 0;
+    public int scrollSpeed = 50;
+
+    public AudioSource[] audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio[idx].Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (idx < posX.Length - 1)
+            {
+                idx++;
+                audio[idx].Play();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (idx > 0)
+            {
+                idx--;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(posX[idx],
+            transform.position.y), scrollSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Script/ScenesManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public string enterScene;
+    public string escapeScene;
+    public bool isEscapeForQuit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Debug.Log("Name Scene: " + enterScene);
+            SceneManager.LoadScene(enterScene);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isEscapeForQuit)
+            {
+                Application.Quit();
+            }
+
+            else
+            {
+                Debug.Log("Name Scene: " + escapeScene);
+                SceneManager.LoadScene(escapeScene);
+            }
+        }
+        
+    }
+}
